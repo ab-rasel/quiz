0427fa117e87a675485f4a29fb7040e45543885f
Ab Rasel <abrasel600@gmail.com>
2021-02-17 01:58:12 +0600

    show result working

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ var quiz = Array(
             b: 'hypertext prgoraming language',
             c: 'hypertext multi lanuage',
         },
-        correctAns : 'a',
+        correctAns : 'b',
     },
     {
         question: 'How do you create an object in JavaScript?',
@@ -46,9 +46,14 @@ function setMarkup(){
     quiz.forEach((currentvalue, currentIndex) =>{
          var ansList = [];
             for(let ind in currentvalue.ansList){
+                 var checked = '';
+                 if(ind === 'a'){
+                    checked = 'checked'
+                 }
+                // alert(ind);
                 ansList.push(
                     `<div class="q-list">
-                        <input type="radio" name="question${currentIndex}" id="${ind}${currentIndex}">
+                        <input type="radio" name="question${currentIndex}" value="${ind}" id="${ind}${currentIndex}" ${checked}>
                         <label for="${ind}${currentIndex}">${currentvalue.ansList[ind]}</label>
                     </div>
                     `
@@ -57,7 +62,9 @@ function setMarkup(){
         questionList.push(
             `<div class="question-item">
                 <h3> ${currentvalue.question} </h3>
-                ${ansList}
+                <div class="ans-items">
+                     ${ansList}
+                </div>
             </div>`
         );
     });
@@ -70,5 +77,27 @@ setMarkup();
 // result
  
 function quizResult(){
-    
+    const answerContainers = quizWrapper.querySelectorAll('.ans-items');
+    var resultContainer = document.getElementById('resultContainer');
+    let correctNumber = 0;
+
+    quiz.forEach((currentvalue, currentIndex) =>{
+        var  answerContainer = answerContainers[currentIndex];
+
+        var selector = `input[name=question${currentIndex}]:checked`;
+        var userAnswer =(answerContainer.querySelector(selector));
+        var ans = userAnswer.value;
+        var correct = currentvalue.correctAns;
+        if(ans == correct){
+            correctNumber++;
+        }
+
+    });
+    alert(correctNumber);
 }
+
+// quizResult();
+
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', quizResult);
+
